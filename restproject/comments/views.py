d59bec328258a1155fea5a3457bb4a562da0aa92
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-12 16:34:04 +0600

    change serializer.save() to 3 fields

--- restproject/comments/views.py
+++ restproject/comments/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from api.models import Book
 from .models import Comment
 from rest_framework.views import APIView
-from .serializers import BookDetailSerializer, CommentSerializer
+from .serializers import BookDetailSerializer, CommentSerializer, CommentCreateSerializer
 
 
 # Create your views here.
@@ -21,9 +21,11 @@ class BookDetailView(APIView):
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
-        serializer = CommentSerializer(data=request.data)
+        serializer = CommentCreateSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            book = Book.objects.get(id=kwargs['book_id'])
+            text = serializer.data.get('text')
+            Comment.objects.create(book=book, text=text, user=request.user)
             return Response({"data": "comment create succesfully!"})
         return Response(serializer.errors)
 
