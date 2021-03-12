9a52d6f3a86de48b5322a1f4b7dde8e49900f0ad
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-12 17:53:49 +0600

    create model Rate

--- restproject/estimate/models.py
+++ restproject/estimate/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from api.models import Book
+
 
 # Create your models here.
+
+
+class Rate(models.Model):
+    star = models.PositiveIntegerField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.book.title
