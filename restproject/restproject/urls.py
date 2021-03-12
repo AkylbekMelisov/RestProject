e3eaacd454c1bdf6f1f46303da9fae337752d2b5
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-12 17:14:26 +0600

    register estimate.urls

--- restproject/restproject/urls.py
+++ restproject/restproject/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('api.urls')),
-                  path('', include('quickstart.urls')),
-                  path('', include('comments.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+    path('', include('quickstart.urls')),
+    path('', include('comments.urls')),
+    path('', include('estimate.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
