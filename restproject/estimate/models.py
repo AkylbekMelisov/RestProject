1375bd62912800f18f240beadfd5a2ee38e445fb
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-15 16:35:25 +0600

    add related_name(rates) for field book in model Rate

--- restproject/estimate/models.py
+++ restproject/estimate/models.py
@@ -7,7 +7,7 @@ from api.models import Book
 
 class Rate(models.Model):
     star = models.PositiveIntegerField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='rates')
 
     def __str__(self):
         return self.book.title
