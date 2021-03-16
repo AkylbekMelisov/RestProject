15d478de74205a58eedee14658786811645905ad
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-16 19:13:09 +0600

    add path for register

--- restproject/accounts/urls.py
+++ restproject/accounts/urls.py
@@ -3,8 +3,11 @@ from .views import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('register', RegisterView)
+router.register('register_set', RegisterView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', RegisterConfirmView.as_view()),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        activate, name='activate'),
 ]
