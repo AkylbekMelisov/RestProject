af3e8c1664bbc0b91b3dd5b9ae6907902b73b523
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-16 19:09:11 +0600

    create models Profile in accounts/models

--- restproject/accounts/models.py
+++ restproject/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length=40)
+    image = models.ImageField(blank=True)
+    age = models.PositiveIntegerField()
+    date_join = models.DateTimeField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
