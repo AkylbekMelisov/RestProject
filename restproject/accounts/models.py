7fd2ba1eca69333d9d1a07ee421d7080f8fd2273
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-03-17 16:59:21 +0600

    add option null=True for field image. add option auto_now_add=True. add field wallet

--- restproject/accounts/models.py
+++ restproject/accounts/models.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
-
 
 class Profile(models.Model):
     full_name = models.CharField(max_length=40)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank=True, null=True)
     age = models.PositiveIntegerField()
-    date_join = models.DateTimeField()
+    date_join = models.DateTimeField(auto_now_add=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.full_name
